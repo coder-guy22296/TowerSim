7b99ec8017c4b6bab236485e149bc0785383a662
Can Yildirim <cyildiri@student.42.us.org>
2017-09-28 00:10:02 -0700

    cleanup debug part 2

--- Enemy.js
+++ Enemy.js
@@ -1,22 +1,18 @@
 function Enemy(name, pos, speed)
 {
-    //console.log('enemy constructor start');
     this.name = name;
     this.pos = pos;
     this.speed = speed;
     this.turnsTillWin = Math.ceil(pos / speed);
-    //console.log(name + ' will win in ' + this.turnsTillWin +' moves');
     this.alive = 1;
 }
 
 Enemy.prototype.move = function()
 {
-    //console.log('enemy move start');
     if (this.alive)
     {
         this.pos -= this.speed;
         this.turnsTillWin -= 1;
-        //console.log(this.name+' has moved');
     }
 };
 
@@ -27,16 +23,9 @@ Enemy.prototype.announce = function()
 
 Enemy.prototype.die = function(attacker)
 {
-    //console.log('enemy die start');
-    //this.announce();
-    //console.log('attacker range: '+attacker.range);
-    //console.log('enemy pos: '+this.pos);
     if (this.alive && this.pos <= attacker.range)
     {   
         this.alive = 0;
-        // console.log('Kill ' + this.name
-        //            + ' at ' + this.pos
-        //            + 'm');
         attacker.targetCnt -= 1;
         return Promise.resolve();
     }
