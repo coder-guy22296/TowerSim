eca59578bbb894618b3e362386beeb6918342439
Can Yildirim <cyildiri@student.42.us.org>
2017-09-28 00:07:13 -0700

    cleaning up debug

--- Tower.js
+++ Tower.js
@@ -1,61 +1,39 @@
 function Tower(range, targets)
 {
-    //console.log('tower constructor start');
     this.range = range;
     this.alive = 1;
     this.targetCnt = targets.length;
     this.game = undefined;
-    //console.log('tower constructor end');
-    //console.log('this tower range: '+this.range);
 }
 
 Tower.prototype.selectTarget = function(targets)
 {
-    //console.log('tower selectTarget start');
-    //console.log('st this tower range: '+this.range);
     var theChosenOne = targets[0];
     var tower = this;
     targets.forEach(function(enemy) {
         // if the enemy is a viable target
-        //console.log('foreach');
-        //enemy.announce();
         if (enemy.alive && enemy.pos <= tower.range)
         {
-            //console.log('viable')
-            //console.log(enemy);
-            //console.log('theChosenOne:');
-            //console.log(theChosenOne);
-            //console.log('tower range: '+tower.range);
-            //get priorities straight
+            //compare to the current target if the current target is alive
             if (enemy.turnsTillWin < theChosenOne.turnsTillWin || theChosenOne.alive === 0)
             {
                 theChosenOne = enemy;
-                //console.log('selected '+enemy.name+' for death');
             }
         }
     });
-    //console.log('selection: ');
-    //console.log(theChosenOne);
     if (theChosenOne.alive === 0 || theChosenOne.pos > tower.range)
     {
-        //console.log('reject promise');
         return Promise.reject();
     }
     else
     {
-        //console.log('final selection: '+theChosenOne.name);
         return Promise.resolve(theChosenOne);
     }
 }
 
 Tower.prototype.killTarget = function(target)
 {
-    //console.log('tower killTarget start');
-    //console.log('the chosen one: '+target.name+target.alive);
     var currentTurn = this.game.turn;
-    //console.log(this);
-    //console.log('killTarget: tower range: '+this.range);        // attackEnemy: tower range: undefined
-    //console.log('this target count: '+this.targetCnt);
     target.die(this)
     .then(function() {
         console.log('turn '+currentTurn+': killed '+target.name+' at '+target.pos+'m');
@@ -66,14 +44,11 @@ Tower.prototype.killTarget = function(target)
 
 function noTargetsAvailable()
 {
-    //console.log('this tower range: '+this.range);
     //console.log('no targets to kill!');
 }
 
 Tower.prototype.attackEnemy = function(targets)
 {
-    //console.log('tower attackEnemy start');
-    //console.log('attackEnemy: tower range: '+this.range);       // attackEnemy: tower range: 50 
     var parent = this;    
     this.selectTarget(targets).then(this.killTarget.bind(this), noTargetsAvailable);
     return Promise.resolve(targets);
--- index.js
+++ index.js
@@ -4,46 +4,30 @@ var TowerDefence = require('./TowerDefence.js')
 
 function targetBadie(game)
 {
-    //console.log('target badie start');
-    //console.log('tb this tower range: '+game.tower.range);
-    //console.log('badies: '+game.badies);
     tower.attackEnemy(game.badies);
-    //console.log('badies: '+game.badies);
     return Promise.resolve(game);
 }
 
 function moveBadies(game)
 {
-    //console.log('mb this tower range: '+game.tower.range);
-    //console.log('badies.lenght'+game.badies);
     for (var i = 0; i < game.badies.length; i++)
     {
         var enemy = game.badies[i];
         enemy.move();
-        //enemy.announce();
         if (enemy.pos <= 0)
         {
-            //console.log(enemy.name+' killed tower at '+enemy.pos);
             tower.alive = 0;
-            //console.log('tower dead');
             break;
         }
     }
-    //console.log('move badies done');
     return Promise.resolve(game);
 }
 
 function gameLoop(game)
 {
-    //console.log('this tower range: '+game.tower.range);
-    //console.log('this target count: '+game.tower.targetCnt);
     targetBadie(game)
     .then(moveBadies)
     .then(function(game) {
-        //console.log('end turn');
-        //console.log('tower range: '+game.tower.range);
-        //console.log('tower targets: '+game.tower.targetCnt);
-        //console.log('tower alive: '+game.tower.alive);
         if (game.tower.alive && game.tower.targetCnt > 0)
         {
             game.turn += 1;
@@ -68,7 +52,5 @@ badies.push( new Enemy('bill', 100, 10) );
 badies.push( new Enemy('mike', 30, 20) );
 var tower = new Tower(50, badies);
 var game = new TowerDefence(tower, badies);
-//console.log('tower: '+tower.targetCnt);
-//console.log('GAME START!!');
 console.log('firing range: '+tower.range);
 gameLoop(game);
